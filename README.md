437999d8cf61c1028b8812217d471924634ed3b0
lillusory <xueduanli@163.com>
2019-03-03 22:10:02 +0800

    netty心跳机制生产级代码

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Java  Learning   Note And Demo
 
 ## 1.BIO、NIO、AIO
 
+## 2.生产者消费者模型
+
+## 3.netty心跳机制实现
---
+++ src/main/java/netty/heartbeatproduct/AcceptorIdleStateTrigger.java
@@ -0,0 +1,20 @@
+package netty.heartbeatproduct;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+
+public class AcceptorIdleStateTrigger extends ChannelHandlerAdapter {
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.READER_IDLE) {
+                throw new Exception("idle exception");
+            }
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+}
